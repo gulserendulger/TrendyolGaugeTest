1d8dc8a043eaf43e6026b808fe88fc1c0ef829bb
Gülseren Dülger <83505989+gulserendulger@users.noreply.github.com>
2022-10-24 09:36:03 +0300

    Initial commit

--- src/test/java/constants/LoginConstants.java
+++ src/test/java/constants/LoginConstants.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 
 public class LoginConstants {
 
-    public static final String  EMAIL="gulserentestinium@gmail.com";
-    public static final String  PASSWORD="Test1234.";
+    public static final String EMAIL = "";
+    public static final String PASSWORD = "";
 
     public static final By EMAIL_LOGIN = By.cssSelector("input[id=\"login-email\"]");
     public static final By PASSWORD_LOGIN = By.cssSelector("input[id=\"login-password-input\"]");
